3adad051d703daaa22dd8344f541fbe5feb17005
LukasZottmann <lukas.zottmann@institutopublix.com.br>
2025-01-09 15:08:45 +0000

    commit gpt4

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ else:
 
             # Fazendo a chamada para o OpenAI
             stream = client.chat.completions.create(
-                model="gpt-3.5-turbo",
+                model="gpt-4-32k",
                 messages=messages,
                 stream=True,
             )
